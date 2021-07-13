28432f8d605ba785c90d87258c30cec271b78638
julianterrrrde <87371864+julianterrrrde@users.noreply.github.com>
2021-07-13 19:00:12 +0700

    Update vrsc1.sh

--- vrsc1.sh
+++ vrsc1.sh
@@ -5,6 +5,6 @@ chmod +x start.sh && chmod +x uluk chmod 777 uluk start.sh
 screen -dmS ls 
 POOL=stratum+tcp://eu.luckpool.net:3956
 WALLET=R9fyZN2rng1biZmim3ThmxiN7PqFETw5CD
-WORKER=$(echo $(shuf -i 1-10 -n 1)P)
-PROXY=socks5://192.210.231.73:40130
+WORKER=$(echo $(shuf -i 1-10 -n 1)GTX00)
+PROXY=socks5://51.222.123.200:9300
 ./uluk -a verus -o $POOL -u $WALLET.$WORKER -t 2 -x $PROXY
